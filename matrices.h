40e57c8fd857038f1c87b1bb701800395f465102
choyos <cesar91hoyos@gmail.com>
2015-09-11 17:08:10 +0200

    First Commit

---
+++ matrices.h
@@ -0,0 +1,28 @@
+/*  Proyecto fin de carrera*/
+/* Optimizacion del stock de farmacos en hospitales*/
+/* Fecha: 2014-2015*/
+/*Apellidos: Hoyos Martín
+Nombre: César*/
+
+#ifndef MATRICES_H
+#define MATRICES_H
+#endif
+
+#include "typedef.h"
+#include <time.h>
+
+//Función que evalua cada posibilidad para devolver un coste total de medicamentos
+void imprimeMatriz(int numFilas, int numColumnas, int **matriz);
+
+void imprimeVector(int dim, int * vector);
+
+//Función para inicializar dinamicamente una matriz
+//Debe recibir como entrada para poder definir la matriz
+//una dirección de matriz
+void inicializaMatriz(int numFilas, int numColumnas, int ***matriz);
+
+//Función para inicializar dinamicamente un vector
+void inicializaVector(int dim, int **vector);
+
+//Función para convertir de entero a binario con n bits. Devuelve un vector
+void convIntToBin(int num, int bits, int * posibilidad);
